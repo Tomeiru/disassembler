795444f67bed4691ae6b7eb8d094cb26590c5c3d
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-05-23 17:50:20 +0900

    feat: add ideal idea of instruction handling with hardcoded mov

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,49 @@
 mod header;
+mod register;
+use register::Register;
+
 use crate::header::Header;
 use std::{env, fs, process, usize};
+
+fn recognize_mov_immediate_to_register(bytes: &[u8]) -> bool {
+    return bytes[0] & 0b11110000 == 0b10110000;
+}
+
+// TODO: change name
+#[derive(Debug)]
+struct DisassembledInstruction {
+    arguments: Vec<String>,
+    instructions: Vec<u8>,
+}
+
+impl DisassembledInstruction {
+    fn default() -> DisassembledInstruction {
+        return DisassembledInstruction {
+            arguments: vec![],
+            instructions: vec![],
+        };
+    }
+}
+
+fn dissassemble_mov_itr(bytes: &[u8]) -> Result<DisassembledInstruction, &'static str> {
+    let mut result = DisassembledInstruction::default();
+    let word: bool = bytes[0] & 0b00001000 == 0b00001000;
+    if (word && bytes.len() < 3) || (!word && bytes.len() < 2) {
+        return Err("size");
+    }
+    let reg = Register::get(bytes[0] & 0b00000111, word)?;
+    result.arguments.push(reg.to_str().to_string());
+    if word {
+        result
+            .arguments
+            .push(format!("{:04x}", u16::from_le_bytes([bytes[1], bytes[2]])));
+        result.instructions.extend(&bytes[0..3].to_vec());
+        return Ok(result);
+    }
+    result.arguments.push(format!("{:02x}", bytes[1]));
+    result.instructions.extend(&bytes[0..2].to_vec());
+    return Ok(result);
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -27,5 +70,23 @@ fn main() {
     };
     let text_start = header.get_text_position();
     let text = &data[text_start..text_start + header.get_text_size() as usize];
-    dbg!(text);
+    println!(
+        "is instruction mov (immediate to register)? {}",
+        recognize_mov_immediate_to_register(text)
+    );
+
+    let result = dissassemble_mov_itr(text).unwrap();
+    let formatted_instructions: Vec<String> = result
+        .instructions
+        .into_iter()
+        .map(|instruction| format!("{:02x}", instruction))
+        .collect();
+
+    println!(
+        "{:04x}: {:<13} {} {}",
+        0,
+        "mov",
+        formatted_instructions.join(""),
+        result.arguments.join(", ")
+    );
 }
---
+++ src/register.rs
@@ -0,0 +1,94 @@
+//TODO: remove the warning cleaners of dead code when segments is implemented
+
+#[derive(Copy, Clone)]
+#[allow(dead_code)]
+pub enum Register {
+    Ax,
+    Cx,
+    Dx,
+    Bx,
+    Sp,
+    Bp,
+    Si,
+    Di,
+    Al,
+    Cl,
+    Dl,
+    Bl,
+    Ah,
+    Ch,
+    Dh,
+    Bh,
+    Es,
+    Cs,
+    Ss,
+    Ds,
+}
+
+const REGISTERS: [[Register; 8]; 2] = [
+    [
+        Register::Ax,
+        Register::Cx,
+        Register::Dx,
+        Register::Bx,
+        Register::Sp,
+        Register::Bp,
+        Register::Si,
+        Register::Di,
+    ],
+    [
+        Register::Al,
+        Register::Cl,
+        Register::Dl,
+        Register::Bl,
+        Register::Ah,
+        Register::Ch,
+        Register::Dh,
+        Register::Bh,
+    ],
+];
+
+#[allow(dead_code)]
+const SEGMENTS: [Register; 4] = [Register::Es, Register::Cs, Register::Ss, Register::Ds];
+
+impl Register {
+    pub fn get(value: u8, word: bool) -> Result<Register, &'static str> {
+        if value > 8 {
+            return Err("");
+        }
+        return Ok(REGISTERS[usize::from(!word)][usize::from(value)]);
+    }
+
+    #[allow(dead_code)]
+    pub fn get_segment(value: u8) -> Result<Register, &'static str> {
+        if value > 4 {
+            return Err("a register segment with this value does not exist");
+        }
+        return Ok(SEGMENTS[usize::from(value)]);
+    }
+
+    pub fn to_str(&self) -> &'static str {
+        return match self {
+            Register::Ax => "ax",
+            Register::Cx => "cx",
+            Register::Dx => "dx",
+            Register::Bx => "bx",
+            Register::Sp => "sp",
+            Register::Bp => "bp",
+            Register::Si => "si",
+            Register::Di => "di",
+            Register::Al => "al",
+            Register::Cl => "cl",
+            Register::Dl => "dl",
+            Register::Bl => "bl",
+            Register::Ah => "ah",
+            Register::Ch => "ch",
+            Register::Dh => "dh",
+            Register::Bh => "bh",
+            Register::Es => "es",
+            Register::Cs => "cs",
+            Register::Ss => "ss",
+            Register::Ds => "ds",
+        };
+    }
+}
