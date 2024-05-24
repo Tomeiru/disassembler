2dd38b20fe91e1ca6b25ac24ff8ba0c301e5bb4c
Mathieu Brujan <mathieu.brujan@epitech.eu>
2024-05-24 15:51:36 +0900

    tests: add mov::immediate_to_register unit tests

--- src/instruction/mov/immediate_to_register.rs
+++ src/instruction/mov/immediate_to_register.rs
@@ -1,8 +1,6 @@
 use crate::instruction;
 use crate::register;
 
-// TODO: unit testing
-
 fn recognize(bytes: &[u8]) -> bool {
     return bytes[0] & 0b11110000 == 0b10110000;
 }
@@ -31,4 +29,62 @@ pub fn get_instruction() -> instruction::Instruction {
         recognize,
         disassemble,
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::disassemble;
+    use super::recognize;
+
+    #[test]
+    fn correct_sequence_without_word_recognition() {
+        let bytes: [u8; 2] = [0b10110000, 0b11001101];
+        assert_eq!(recognize(&bytes), true);
+    }
+
+    #[test]
+    fn correct_sequence_with_word_recognition() {
+        let bytes: [u8; 2] = [0b10111000, 0b11001101];
+        assert_eq!(recognize(&bytes), true);
+    }
+
+    #[test]
+    fn wrong_sequence_recognition() {
+        let bytes: [u8; 2] = [0b11111000, 0b11001101];
+        assert_eq!(recognize(&bytes), false);
+    }
+
+    #[test]
+    fn correct_sequence_without_word_dissassembly() {
+        let bytes: [u8; 2] = [0b10110000, 0x20];
+        let (arguments, instruction_bytes) = disassemble(&bytes).unwrap();
+        assert_eq!(arguments, ["al".to_string(), "20".to_string()]);
+        assert_eq!(instruction_bytes, bytes);
+    }
+
+    #[test]
+    fn correct_sequence_with_word_dissassembly() {
+        let bytes: [u8; 3] = [0b10111000, 0x8, 0x7];
+        let (arguments, instruction_bytes) = disassemble(&bytes).unwrap();
+        assert_eq!(arguments, ["ax".to_string(), "0708".to_string()]);
+        assert_eq!(instruction_bytes, bytes);
+    }
+
+    #[test]
+    fn incorrect_length_without_word_sequence() {
+        let bytes: [u8; 1] = [0b10110000];
+        assert_eq!(recognize(&bytes), true);
+        let err = disassemble(&bytes).unwrap_err();
+        assert_eq!(err, "size");
+    }
+
+    #[test]
+    fn incorrect_length_with_word_sequence() {
+        let bytes: [u8; 2] = [0b10111000, 0b10111000];
+        assert_eq!(recognize(&bytes), true);
+        let err = disassemble(&bytes[..1]).unwrap_err();
+        assert_eq!(err, "size");
+        let err = disassemble(&bytes).unwrap_err();
+        assert_eq!(err, "size");
+    }
 }
